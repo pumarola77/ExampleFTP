2116146de35a2f8c5ec3985c67a6b61a4a1d444b
pumarola77 <31644700+pumarola77@users.noreply.github.com>
2017-11-05 01:10:37 +0100

    Add files via upload

---
+++ ExampleFTP/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Renci.SshNet;
+using System.IO;
+
+namespace ExampleFTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListFiles();   
+        }
+
+        private static void ListFiles()
+        {
+            String host = "192.168.1.171";
+            String username = "pumarola77";
+            String password = "perico01";
+
+            String pathRemoteDirectory;
+            String remoteDirectory = "/";
+            String pathLocalFile;
+
+            try
+            {
+
+                var client = new SftpClient(host, 2222, username, password);
+                client.Connect();
+
+                if (client.IsConnected)
+                {
+                    // LLegir directori amb els fitxers
+
+                    var sftpClient = client.ListDirectory(remoteDirectory);
+
+                    foreach (var contingut in sftpClient)
+                    {
+                        
+                        if ( contingut.Name.Contains("DELNOT"))
+                        {
+                            pathLocalFile = "C:/Users/Bazinga/Proves/" + contingut.Name;
+                            pathRemoteDirectory = remoteDirectory +  contingut.Name;
+                            
+                            using (Stream fileStream = File.OpenWrite(pathLocalFile))
+                            {
+                                client.DownloadFile(pathRemoteDirectory, fileStream);
+                            }
+                        }
+                    }
+                    
+                    client.Disconnect();
+
+                    //Lectura dels Fitxers d'un Directori. Utilitzant Pattern.
+                    DirectoryInfo di = new DirectoryInfo("C:/Users/Bazinga/Proves");                    
+                    foreach (var fi in di.GetFiles("*DELNOT*"))
+                    {
+                        //Tractament Fitxer DELNOT
+                        pathLocalFile = "C:/Users/Bazinga/Proves/" + fi;
+                        TractamentDELNOT.LecturaDELNOT(pathLocalFile);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception has been caught " + e.ToString());
+            }
+        }
+    }
+
+}
---
+++ ExampleFTP/tractamentDELNOT.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace ExampleFTP
+{
+    class TractamentDELNOT
+    {
+        public static void LecturaDELNOT(String path)
+        {
+            char delimiter = ',';
+            
+            string[] lines = File.ReadAllLines(path);
+
+            foreach (string line in lines)
+            {       
+                // Parteix la linia per el delimitador.
+                String[] substrings = line.Split(delimiter);
+
+                //Console.WriteLine(substrings.Length);
+
+                foreach (var substring in substrings)
+                 {
+                    Console.WriteLine(substring);    
+                 }
+                Console.WriteLine("#########################");
+            }
+
+            // Un cop recollits els valors faria falta el insert
+            // a la taula.
+        }
+    }
+}
